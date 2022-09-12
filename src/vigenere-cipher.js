f2edbc0bde41964ac8c7a2e2a3ad7adb03748d47
Liavontsyeu Siarhei <leontevsi@gmail.com>
2022-09-12 10:44:32 +0300

    feat: resolve vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,33 +3,80 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification = true) {
+    this.modification = modification;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  checkParams(msg, key) {
+    if (!msg || !key) throw new Error('Incorrect arguments!');
+  }
+
+  isLetter(l) {
+    return l.toUpperCase() !== l.toLowerCase();
+  }
+
+  coding(method, input, key) {
+    let msg = input.toUpperCase();
+    let keyMsg = key
+      .repeat(Math.ceil(msg.length / key.length))
+      .slice(0, msg.length)
+      .toUpperCase();
+
+    const codingArr = [];
+    for (let i = 0, j = 0; i < msg.length; i++) {
+      if (this.isLetter(msg[i])) {
+        if (method === 'encrypt') {
+          codingArr.push(
+            String.fromCharCode(
+              ((msg.charCodeAt(i) + keyMsg.charCodeAt(j)) % 26) + 1 + 64
+            )
+          );
+          j++;
+        } else {
+          codingArr.push(
+            String.fromCharCode(
+              ((msg.charCodeAt(i) + 26 - keyMsg.charCodeAt(j)) % 26) + 1 + 64
+            )
+          );
+          j++;
+        }
+      } else {
+        codingArr.push(msg[i]);
+      }
+    }
+    return this.modification
+      ? codingArr.join('')
+      : codingArr.reverse().join('');
+  }
+
+  encrypt(message, key) {
+    this.checkParams(message, key);
+    return this.coding('encrypt', message, key);
+  }
+
+  decrypt(message, key) {
+    this.checkParams(message, key);
+    return this.coding('decrypt', message, key);
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
